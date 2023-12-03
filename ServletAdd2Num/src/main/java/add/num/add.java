3ad46a3da6b13eda7574c9b340868b3d5a43b615
Pavan Koppula <145316378+pavan-koppula@users.noreply.github.com>
2023-12-03 12:48:43 +0530

    Servlet_Ex

---
+++ ServletAdd2Num/src/main/java/add/num/add.java
@@ -0,0 +1,41 @@
+package add.num;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class add
+ */
+@WebServlet("/adding")
+public class add extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public add() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int i = Integer.parseInt(request.getParameter("num1"));
+		int j = Integer.parseInt(request.getParameter("num2"));
+		
+		int k= i+j;
+		System.out.println("Total is="+k);
+		
+		PrintWriter out=response.getWriter();
+		out.println("Result is="+k);
+	}
+
+}
